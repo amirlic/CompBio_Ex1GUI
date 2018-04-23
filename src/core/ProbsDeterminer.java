05d28389556a93685310ed2955bc5bce24b775ad
amirlic <lichtea3@macs.biu.ac.il>
2018-04-23 17:04:50 +0300

    Update ProbsDeterminer.java

--- src/core/ProbsDeterminer.java
+++ src/core/ProbsDeterminer.java
@@ -1,11 +1,13 @@
 package core;
 
+// class ProbsDeterminer
 public class ProbsDeterminer {
     private double treeExistenceProb;
     private double treeGrowingProb;
     private double fireProb;
     private double spreadingProb;
 
+    // ctor
     public ProbsDeterminer(double treeExistenceProb, double treeGrowingProb, double fireProb, double spreadingProb){
         this.fireProb = fireProb;
         this.spreadingProb = spreadingProb;
@@ -13,30 +15,37 @@ public class ProbsDeterminer {
         this.treeExistenceProb = treeExistenceProb;
     }
 
+    // getFireProb
     public double getFireProb() {
         return fireProb;
     }
 
+    // getSpreadingProb
     public double getSpreadingProb() {
         return spreadingProb;
     }
-
+    
+    // getTreeGrowingProb
     public double getTreeGrowingProb() {
         return treeGrowingProb;
     }
-
+    
+    // getTreeExistenceProb
     public double getTreeExistenceProb() {
         return treeExistenceProb;
     }
 
+    // setFireProb
     public void setFireProb(double fireProb) {
         this.fireProb = fireProb;
     }
 
+    //setSpreadingProb
     public void setSpreadingProb(double spreadingProb) {
         this.spreadingProb = spreadingProb;
     }
-
+    
+    // setTreeGrowingProb
     public void setTreeGrowingProb(double treeGrowingProb) {
         this.treeGrowingProb = treeGrowingProb;
     }
